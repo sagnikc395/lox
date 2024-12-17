809203701e0e849b3b7f4f32329a548b418fdd25
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-17 17:42:40 +0530

    src: scanning parenthesis

--- src/main.rs
+++ src/main.rs
@@ -22,16 +22,29 @@ fn main() {
                 String::new()
             });
 
-            // Uncomment this block to pass the first stage
-            if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
-            }
+            // // Uncomment this block to pass the first stage
+            // if !file_contents.is_empty() {
+            //     panic!("Scanner not implemented");
+            // } else {
+            //     println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+            // }
+            tokenize(&file_contents);
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
             return;
         }
     }
+}
+
+fn tokenize(input: &str) {
+    //for the given input char , match to tokens
+    for char in input.chars() {
+        match char {
+            '(' => println!("LEFT_PAREN ( null"),
+            ')' => println!("RIGHT_PAREN ) null"),
+            _ => {} //others ignored for now
+        }
+    }
+    println!("EOF null");
 }
