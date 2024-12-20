1de3fae4e15fe8e30f0969b4b261f2eb9ccd2f5a
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-21 03:28:16 +0530

    src: brain not braining

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::fs;
+use std::process;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        println!("Usage: lox [script]");
+        process::exit(64);
+    } else if args.len() == 1 {
+        run_file(&args[0]);
+    } else {
+        run_prompt();
+    }
+}
+
+fn run_file(path: &String) {
+    let data = fs::read_to_string(path).expect("Failed to read the file");
+    run(data);
+}
+
+fn run_prompt() {
+    
 }
