1f94018089ec6016eb6f0a1ff0a55616ecbae022
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-01-04 20:49:25 +0530

    src: wrtier the abstract base class interface

--- lox/tool/genast.py
+++ lox/tool/genast.py
@@ -1,8 +1,23 @@
 import sys
 import os
+from typing import List
 
 
 class GenerateAST:
+    @staticmethod
+    def defineAST(outputdir: str, baseName: str, types: List[str]):
+        try:
+            path = os.path.join(outputdir, f"{baseName}.py")
+
+            with open(path, "w", encoding="utf-8") as writer:
+                writer.write("from abc import ABC, abstractmethod")
+                writer.write("\n")
+                writer.write(f"class {baseName}(ABC):\n")
+                writer.write("\t")
+
+        except Exception as e:
+            print(e)
+
     # script away your syntax trees from this
     @staticmethod
     def main():
@@ -11,6 +26,17 @@ class GenerateAST:
             if len(args) != 1:
                 print("Usage: genast <output directory>")
                 os.exit(64)
+            outputdir = args[0]
+            GenerateAST.defineAST(
+                outputdir,
+                "Expr",
+                [
+                    "Binary   : Expr left, Token operator, Expr right",
+                    "Grouping : Expr expression",
+                    "Literal  : Object value",
+                    "Unary    : Token operator, Expr right",
+                ],
+            )
 
         except Exception as e:
             print(e)
