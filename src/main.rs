b9fb74e72f377c05faef186a5fe68c82620d945a
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-02-03 18:19:21 +0530

    src: barebones lox

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+use std::fs;
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("hello world");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
+        return;
+    }
+
+    let command = &args[1];
+    let filename = &args[2];
+
+    match command.as_str() {
+        "tokenize" => {
+            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
+                writeln!(io::stderr(), "Failed to read the file {}", filename).unwrap();
+                String::new()
+            });
+            if !file_contents.is_empty() {
+                panic!("Scanner not implemented");
+            } else {
+                println!("EOF null");
+            }
+        }
+        _ => {
+            writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
+            return;
+        }
+    }
 }
